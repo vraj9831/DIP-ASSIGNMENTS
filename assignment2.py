33844ca0202192df8c77c8fe1766c8b07d314951
vraj9831 <59813381+vraj9831@users.noreply.github.com>
2022-04-28 17:02:53 +0530

    Create assignment2.py

---
+++ assignment2.py
@@ -0,0 +1,17 @@
+  import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+img = cv.imread('jeff.jpg',0)
+edges = cv.Canny(img,100,200)
+plt.subplot(121)
+plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'),
+plt.xticks([])
+plt.yticks([])
+plt.subplot(122)
+plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image')
+plt.xticks([])
+plt.yticks([])
+plt.show()
+#Canny edge Detection
